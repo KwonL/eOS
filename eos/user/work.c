25c4f6636112784fcbfba9d09d1bf9e5ff82e7cc
unknown <dlrnjsgud322@gmail.com>
2018-05-30 00:03:39 +0900

    feat: Add user task for project 2

--- eos/user/work.c
+++ eos/user/work.c
@@ -5,6 +5,9 @@ static eos_tcb_t tcb2;
 static int8u_t stack1[8096];
 static int8u_t stack2[8096];
 
+/*
+ * these tasks are for project 2
+ */
 /* task1 function - print number 1 to 20 repeatedly */
 void print_number() {
 	int i = 0;
@@ -23,8 +26,33 @@ void print_alphabet() {
 		if (i == 122) { i = 96; }
 	}
 }
+/////////////////////////////////////////////////
+
+/*
+ * these tasks are for project 3
+ */
+void task1() {
+	while (1) { PRINT("A\n"); eos_sleep(0); }
+}
+
+void task2() {
+	while (1) { PRINT("B\n"); eos_sleep(0); }
+}
+
+void task2() {
+	while (1) { PRINT("C\n"); eos_sleep(0); }
+}
+
 
 void eos_user_main() {
-	eos_create_task(&tcb1, (addr_t)stack1, 8096, print_number, NULL, 0);
-	eos_create_task(&tcb2, (addr_t)stack2, 8096, print_alphabet, NULL, 0);
+	// project 2
+	// eos_create_task(&tcb1, (addr_t)stack1, 8096, print_number, NULL, 0);
+	// eos_create_task(&tcb2, (addr_t)stack2, 8096, print_alphabet, NULL, 0);
+
+	eos_create_task(&tcb1, stack1, STACK_SIZE, task1 NULL, 1);
+	eos_set_period(&tcb1, 2);
+	eos_create_task(&tcb2, stack2, STACK_SIZE, task2 NULL, 10);
+	eos_set_period(&tcb2, 4);
+	eos_create_task(&tcb3, stack3, STACK_SIZE, task3 NULL, 50);
+	eos_set_period(&tcb3, 8);
 }
